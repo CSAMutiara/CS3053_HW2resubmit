98fb0f2f146fb9a2a70be4b18cd1aacdb2ffa32e
Ayesha Mutiara <a100017@aup.edu>
2018-03-04 16:48:59 +0000

    reupload all files to deploy on heroku

---
+++ lib/hangperson_game.rb
@@ -0,0 +1,69 @@
+class HangpersonGame
+
+  # add the necessary class methods, attributes, etc. here
+  # to make the tests in spec/hangperson_game_spec.rb pass.
+  attr_accessor(:word, :guesses, :wrong_guesses, :valid)
+
+  # Get a word from remote "random word" service
+
+  # def initialize()
+  # end
+  def initialize(word)
+    @word = word.downcase
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  # You can test it by running $ bundle exec irb -I. -r app.rb
+  # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
+  #  => "cooking"   <-- some random word
+  def self.get_random_word
+    require 'uri'
+    require 'net/http'
+    uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
+    Net::HTTP.new('watchout4snakes.com').start { |http|
+      return http.post(uri, "").body
+    }
+  end
+  
+  def guess(c)
+    
+    #raise ArgumentError, "Invalid Argument." letter.is_a? String
+    raise ArgumentError, "Invalid Argument." unless c =~ /[a-z]/i
+    
+    valid = true
+    
+    if @word =~ /[#{c}]/i
+      unless @guesses =~ /[#{c}]/i
+        @guesses += c
+      else
+        valid = false
+      end
+    else
+      unless @wrong_guesses =~ /[#{c}]/i
+        @wrong_guesses += c
+      else
+        valid = false
+      end
+    end
+    
+    valid
+    
+  end
+    
+  def word_with_guesses
+    @word.gsub(/[^#{@guesses + '#'}]/i, '-')
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7
+      return :lose
+    end
+    if word_with_guesses =~ /[\-]/
+      return :play
+    else
+      return :win
+    end
+  end
+
+end
